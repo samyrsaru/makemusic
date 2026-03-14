5cbf395e00779730b0c7fef2789db0406db8bd23
Samyr Saru <samyrsaru@gmail.com>
2026-03-14 18:10:03 +0200

    Center demo tracks - change grid to 2 columns

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -310,7 +310,7 @@ function Home() {
               </p>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-3xl mx-auto">
               {demoSongs.map((song) => (
                 <div
                   key={song.id}
